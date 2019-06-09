e8d7d15fc9f14bf20de68c34746eea7478eafbe0
hardyzhao <43235997+hardyzhao@users.noreply.github.com>
2019-06-09 15:06:24 +0800

    attack

---
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 50;
+    public float speed = 50;
+    private Transform target;
+    
+    public void setTatget(Transform _target)
+    {
+        this.target = _target;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/godA.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class godA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> enemys = new List<GameObject>();
+    private float attackRate = 1;
+    private float timer = 0;
+    public GameObject bulletPrefab;
+    public Transform firePos;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            enemys.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            enemys.Remove(other.gameObject);
+        }
+    }
+    
+    void setAttackRate(float _attackRate)
+    {
+        this.attackRate = _attackRate;
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+        if (timer <= attackRate && enemys.Count > 0)
+        {
+            attack();
+            timer += attackRate;
+        }
+        
+        
+    }
+
+    void attack()
+    {
+         GameObject bullet = GameObject.Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        bullet.GetComponent<Bullet>().setTatget(enemys[0].transform);
+        
+    }
+}
